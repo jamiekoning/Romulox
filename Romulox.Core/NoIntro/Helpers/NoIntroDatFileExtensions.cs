f048f3d2870346819eaa69b2a31ab8ddd80ff84e
Jamie Koning <8761556+jamiekoning@users.noreply.github.com>
2019-07-22 14:26:47 -0600

    syntax cleanup

--- Romulox.Core/NoIntro/Helpers/NoIntroDatFileExtensions.cs
+++ Romulox.Core/NoIntro/Helpers/NoIntroDatFileExtensions.cs
@@ -19,7 +19,6 @@ namespace Romulox.Core.NoIntro.Helpers
                         return game;
                     }
                 }
-                
             }
 
             return null;
@@ -36,7 +35,6 @@ namespace Romulox.Core.NoIntro.Helpers
                         return rom;
                     }
                 }
-                
             }
 
             return null;
